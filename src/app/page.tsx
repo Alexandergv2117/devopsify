bfc83b4e2f3573b304ef624f40752b7191423a9d
Alexander Garcia <alexandergv2117@gmail.com>
2024-11-15 15:41:59 -0500

    feat: list containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,8 @@
-"use client";
+import { docker } from "@/lib/docker";
+import { Button } from "@nextui-org/react";
 
-import Link from "next/link";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Chip,
-  Tab,
-  Tabs,
-} from "@nextui-org/react";
-
-const PROJECTS = [
-  {
-    id: 1,
-    name: "E-commerce Platform",
-    status: "Active",
-    environments: [
-      { name: "Staging", status: "Running", lastDeployed: "1 day ago" },
-      { name: "Production", status: "Running", lastDeployed: "3 days ago" },
-    ],
-  },
-  {
-    id: 2,
-    name: "Blog Engine",
-    status: "Inactive",
-    environments: [
-      { name: "Staging", status: "Stopped", lastDeployed: "1 week ago" },
-      { name: "Production", status: "Running", lastDeployed: "2 weeks ago" },
-    ],
-  },
-  {
-    id: 3,
-    name: "Mobile App Backend",
-    status: "Active",
-    environments: [
-      { name: "Staging", status: "Running", lastDeployed: "2 hours ago" },
-      { name: "Production", status: "Running", lastDeployed: "1 day ago" },
-    ],
-  },
-  {
-    id: 4,
-    name: "Customer Portal",
-    status: "Active",
-    environments: [
-      { name: "Staging", status: "Failed", lastDeployed: "4 hours ago" },
-      { name: "Production", status: "Running", lastDeployed: "2 days ago" },
-    ],
-  },
-];
-
-export default function Home() {
+export default async function Home() {
+  const containers = await docker.listContainers();
   return (
     <div className="flex flex-col gap-10 mt-2 w-full">
       <section className="flex flex-row justify-between items-center my-6">
@@ -59,49 +11,31 @@ export default function Home() {
           Create Project
         </Button>
       </section>
-      <section className="w-full grid grid-cols-3 gap-8">
-        {PROJECTS.map((project, i) => (
-          // <Link key={i} href={`/projects/${i}`}>
-          <Card className="w-full h-full min-h-60 bg-white/10" key={i}>
-            <CardHeader className="flex justify-between w-full">
-              <Link
-                href={`/projects/${project.id}`}
-                className="hover:text-white/60"
-              >
-                <h2 className="font-semibold text-2xl">{project.name}</h2>
-              </Link>
-              <Chip color={project.status === "Active" ? "success" : "danger"}>
-                {project.status}
-              </Chip>
-            </CardHeader>
-            <CardBody>
-              <Tabs aria-label="Environments">
-                {project.environments.map((env, j) => (
-                  <Tab key={j} title={env.name}>
-                    <Link href={`/projects/${project.id}?env=${env.name}`}>
-                      <Card className="w-full bg-white/10">
-                        <CardBody className="gap-4">
-                          <span className="flex justify-between items-center">
-                            <p>{env.name}</p>
-                            <Chip
-                              color={
-                                env.status === "Running" ? "success" : "danger"
-                              }
-                            >
-                              {env.status}
-                            </Chip>
-                          </span>
-                          <p>Last Deployed: {env.lastDeployed}</p>
-                        </CardBody>
-                      </Card>
-                    </Link>
-                  </Tab>
-                ))}
-              </Tabs>
-            </CardBody>
-          </Card>
-          // </Link>
-        ))}
+      <section className="grid grid-cols-4 gap-6">
+        <h1 className="col-span-4 text-2xl font-semibold">
+          Containers ({containers.length})
+        </h1>
+        <div>
+          {containers.map((container) => (
+            <article
+              key={container.Id}
+              className="flex flex-col rounded-lg border border-white/80 p-6 gap-5"
+            >
+              <h1>{container.Names.splice(0, 1)[0].replace("/", "")}</h1>
+              <span>{container.Status}</span>
+              <span>
+                {new Date(container.Created * 1000).toLocaleString("en-ES", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  second: "2-digit",
+                })}
+              </span>
+            </article>
+          ))}
+        </div>
       </section>
     </div>
   );
